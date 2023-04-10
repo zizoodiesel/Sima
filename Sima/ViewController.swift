372613bf65eed4a13f130357c18a32fdf89faeba
Zied Gannoun <zied.gannoun@nouvelware.com>
2023-04-10 14:57:22 +0100

    Initial Commit

---
+++ Sima/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sima
+//
+//  Created by Zizoo diesel on 10/4/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
